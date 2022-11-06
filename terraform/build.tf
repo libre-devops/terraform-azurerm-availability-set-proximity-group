023010405bfb7ed08195d7a42b69a2b3387beada
Craig Thacker <craigthackerx@gmail.com>
2022-11-06 22:32:48 +0000

    Update module

--- terraform/build.tf
+++ terraform/build.tf
@@ -52,7 +52,7 @@ module "lnx_vm_availability_set" {
   vm_os_simple       = "RHEL8Gen2"
   vm_os_disk_size_gb = "256"
 
-  asg_name            = "asg-${element(regexall("[a-z]+", element(module.lnx_vm.vm_name, 0)), 0)}-${var.short}-${var.loc}-${terraform.workspace}-01" //asg-vmldoeuwdev-ldo-euw-dev-01 - Regex strips all numbers from string
+  asg_name            = "asg-${element(regexall("[a-z]+", element(module.lnx_vm_availability_set, 0)), 0)}-${var.short}-${var.loc}-${terraform.workspace}-01" //asg-vmldoeuwdev-ldo-euw-dev-01 - Regex strips all numbers from string
   availability_set_id = module.availability_set.availability_set_id
   availability_zone   = null
 
@@ -63,56 +63,6 @@ module "lnx_vm_availability_set" {
   subnet_id            = element(values(module.network.subnets_ids), 0)
   storage_account_type = "Standard_LRS"
   identity_type        = "SystemAssigned"
-}
-
-module "lnx_vm_prox_group" {
-  source = "registry.terraform.io/libre-devops/linux-vm/azurerm"
-
-  rg_name  = module.rg.rg_name
-  location = module.rg.rg_location
-  tags     = module.rg.rg_tags
-
-  vm_amount          = 1
-  vm_hostname        = "lnx${var.short}${var.loc}${terraform.workspace}"
-  vm_size            = "Standard_B2ms"
-  vm_os_simple       = "RHEL8Gen2"
-  vm_os_disk_size_gb = "256"
-
-  asg_name                     = "asg-${element(regexall("[a-z]+", element(module.lnx_vm.vm_name, 0)), 0)}-${var.short}-${var.loc}-${terraform.workspace}-01" //asg-vmldoeuwdev-ldo-euw-dev-01 - Regex strips all numbers from string
-  proximity_placement_group_id = module.proximity_placement_group.proximity_placement_group_id
-  availability_zone            = "alternate"
-
-  admin_username = "LibreDevOpsAdmin"
-  admin_password = data.azurerm_key_vault_secret.mgmt_local_admin_pwd.value
-  ssh_public_key = data.azurerm_ssh_public_key.mgmt_ssh_key.public_key
-
-  subnet_id            = element(values(module.network.subnets_ids), 0)
-  storage_account_type = "Standard_LRS"
-  identity_type        = "SystemAssigned"
-}
-
-
-variable "custom_nsg_rules" {
-  default = {
-    "AllowVNetInbound" = { priority = "110", direction = "Inbound", access = "Allow", source_address_prefix = "VirtualNetwork", destination_address_prefix = "VirtualNetwork" },
-  }
-  description = "Rules to be added to nsg"
-}
-
-resource "azurerm_network_security_rule" "allow_rules" {
-  for_each = var.custom_nsg_rules
-
-  name                        = each.key
-  priority                    = each.value.priority
-  direction                   = each.value.direction
-  access                      = each.value.access
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = each.value.source_address_prefix
-  destination_address_prefix  = each.value.destination_address_prefix
-  resource_group_name         = module.rg.rg_name
-  network_security_group_name = module.nsg.nsg_name
 }
 
 module "availability_set" {
