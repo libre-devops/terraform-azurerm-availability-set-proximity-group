fa89b9fee0959350cc211b72738797d1d4644727
Craig Thacker <craigthackerx@gmail.com>
2022-11-06 22:30:11 +0000

    Update module

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "availability_set_id" {
   description = "The name of an availability set"
-  value       = var.create_proximity_placement_group == true && var.create_proximity_placement_group != null && var.create_availability_set == true && var.create_availability_set != null ? azurerm_availability_set.availability_set_with_proximity_placement_group[count.index].id : azurerm_availability_set.availability_set[count.index].id
+  value       = var.create_proximity_placement_group == true && var.create_proximity_placement_group != null && var.create_availability_set == true && var.create_availability_set != null ? azurerm_availability_set.availability_set_with_proximity_placement_group.*.id : azurerm_availability_set.availability_set.*.id
 }
 
 output "availability_set_name" {
   description = "The name of an availability set"
-  value       = var.create_proximity_placement_group == true && var.create_proximity_placement_group != null && var.create_availability_set == true && var.create_availability_set != null ? azurerm_availability_set.availability_set_with_proximity_placement_group[count.index].name : azurerm_availability_set.availability_set[count.index].name
+  value       = var.create_proximity_placement_group == true && var.create_proximity_placement_group != null && var.create_availability_set == true && var.create_availability_set != null ? azurerm_availability_set.availability_set_with_proximity_placement_group.*.name : azurerm_availability_set.availability_set.*.name
 }
 
 output "proximity_placement_group_id" {
   description = "The id of a proximity placement group"
-  value       = var.create_proximity_placement_group == true && var.create_proximity_placement_group != null ? azurerm_proximity_placement_group.proximity_placement_group[count.index].id : null
+  value       = var.create_proximity_placement_group == true && var.create_proximity_placement_group != null ? azurerm_proximity_placement_group.proximity_placement_group.*.id : null
 }
 
 output "proximity_placement_group_name" {
   description = "The name of a proximity placement group"
-  value       = var.create_proximity_placement_group == true && var.create_proximity_placement_group != null ? azurerm_proximity_placement_group.proximity_placement_group[count.index].name : null
+  value       = var.create_proximity_placement_group == true && var.create_proximity_placement_group != null ? azurerm_proximity_placement_group.proximity_placement_group.*.name : null
 }
